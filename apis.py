5d4c9b427eaf34763275378c5c1e1bc9b752b49d
lidong <493024318@qq.com>
2018-06-08 09:44:45 +0800

    dsad

--- apis.py
+++ apis.py
@@ -1,13 +1,2 @@
 print("hello")
-<<<<<<< HEAD
 print("hello")
-print("hello")
-print("hello")
-print("123")
-
-print("123")
-print("123")
-print("123")
-=======
-print("this is a cilent")
->>>>>>> 1538a65baf24328af6c150a2ff3e9e2d33c7cf66
